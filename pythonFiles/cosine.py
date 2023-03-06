62a3f43d7d1fbbc8b330e7f810fc1fb6d2ff0aca
mishra1604 <aaryan.mishra.1604@gmail.com>
2023-03-06 16:57:34 +0000

    added adjusted cosine similarity

--- pythonFiles/cosine.py
+++ pythonFiles/cosine.py
@@ -6,45 +6,53 @@ import time
 
 # find the cosine similarity between users in the sqlite3 database
 conn = sqlite3.connect( 'comp3208_train.db' )
-
-
-def cosine_similarity_users(user1, user2 ):
-    c = conn.cursor()
-    c.execute( 'SELECT Rating FROM example_table WHERE UserID = ?', (user1,) )
-    user1_ratings = c.fetchall()
-    c.execute( 'SELECT Rating FROM example_table WHERE UserID = ?', (user2,) )
-    user2_ratings = c.fetchall()
-    if len(user1_ratings) > 0 and len(user2_ratings) > 0:
-        numerator = sum([user1_ratings[i][0] * user2_ratings[i][0] for i in range(len(user1_ratings))])
-        denominator = ((sum([rating[0] ** 2 for rating in user1_ratings]) ** 0.5)
-                       * (sum([rating[0] ** 2 for rating in user2_ratings]) ** 0.5))
-        return numerator / denominator
-    else:
-        return 0
     
-# find the cosine similarity between items in the sqlite3 database
-def cosine_similarity_items(item1, item2 ):
-    c = conn.cursor()
-    c.execute( 'SELECT Rating FROM example_table WHERE ItemID = ?', (item1,) )
-    item1_ratings = c.fetchall()
+# create a dictionary for average rating of each user
+usr_avg = {}
+def user_avg_rating():
+    for x in range(1, 944):
+        c = conn.cursor()
+        c.execute( 'SELECT AVG(Rating) FROM example_table WHERE UserID = ?', (x,) )
+        avg = c.fetchone()[0]
+        usr_avg[x] = avg
 
-    c.execute( 'SELECT Rating FROM example_table WHERE ItemID = ?', (item2,) )
-    item2_ratings = c.fetchall()
-
-    smallerNumber = 0
-    if len(item1_ratings) > len(item2_ratings):
-        smallerNumber = len(item2_ratings)
-    else:
-        smallerNumber = len(item1_ratings)
+user_avg_rating()
     
-    if len(item1_ratings) > 0 and len(item2_ratings) > 0:
-        numerator = sum([item1_ratings[i][0] * item2_ratings[i][0] for i in range(smallerNumber)])
-        denominator = ((sum([rating[0] ** 2 for rating in item1_ratings]) ** 0.5)
-                       * (sum([rating[0] ** 2 for rating in item2_ratings]) ** 0.5))
-        return numerator / denominator
-    else:
-        return 0
+# find the adjusted cosine similarity between items in the sqlite3 database
+def cosine_similarity_items(item1, item2):
+    c = conn.cursor()
+    c.execute( 'SELECT UserID, Rating FROM example_table WHERE ItemID = ?', (item1,) )
+    item1 = c.fetchall()
+    item1_ratings = [i[1] for i in item1]
+    item1_user = [i[0] for i in item1]
 
+    c.execute( 'SELECT UserID, Rating FROM example_table WHERE ItemID = ?', (item2,) )
+    item2 = c.fetchall()
+    item2_ratings = [i[1] for i in item2]
+    item2_user = [i[0] for i in item2]
+
+    item1Rating = []
+    item2Rating = []
+    userList = []
+    largerList = max(len(item1_ratings), len(item2_ratings))
+    for i in range( largerList ):
+        if len(item1_ratings) > len(item2_ratings):
+            if item1_user[i] in item2_user:
+                item1Rating.append(item1_ratings[i])
+                item2Rating.append(item2_ratings[item2_user.index(item1_user[i])])
+                userList.append(item1_user[i])
+        else:
+            if item2_user[i] in item1_user:
+                item1Rating.append(item1_ratings[item1_user.index(item2_user[i])])
+                item2Rating.append(item2_ratings[i])
+                userList.append(item2_user[i])
+    
+    numerator = sum([(item1Rating[i] - usr_avg[userList[i]]) * (item2Rating[i] - usr_avg[userList[i]]) for i in range(len(userList))])
+    denominator = math.sqrt(sum([(item1Rating[i] - usr_avg[userList[i]])**2 for i in range(len(userList))])) * math.sqrt(sum([(item2Rating[i] - usr_avg[userList[i]])**2 for i in range(len(userList))]))
+    if denominator == 0:
+        return 0
+    else:
+        return numerator / denominator
 
 # k nearest neighbours to an item
 def k_nearest_neighbours(user, item, k):
@@ -53,7 +61,7 @@ def k_nearest_neighbours(user, item, k):
     duplicate_items = c.fetchall()
     items = list(set(duplicate_items))
     items = [item[0] for item in items]
-    similarities = [cosine_similarity_items(item, item2) for item2 in items]
+    similarities = [cosine_similarity_items(item, item2, user) for item2 in items]
     similarities = np.array(similarities)
     items = np.array(items)
     sorted_indices = np.argsort(similarities)
@@ -64,7 +72,7 @@ def predict_rating(userID, itemID, k=5):
     c = conn.cursor()
     neighbours = k_nearest_neighbours(userID, itemID, k)
     userRatingForNeighbour = []
-    print(neighbours)
+    # print(neighbours)
     for neighbour in neighbours:
         c.execute("SELECT Rating FROM example_table WHERE UserID = ? AND ItemID = ?", (userID, int(neighbour)))
         rating = c.fetchone()
@@ -73,8 +81,41 @@ def predict_rating(userID, itemID, k=5):
         else:
             userRatingForNeighbour.append(0)
 
-    Item_similarity_with_neighbour = [cosine_similarity_items(itemID, int(neighbour)) for neighbour in neighbours]
+    Item_similarity_with_neighbour = [cosine_similarity_items(itemID, int(neighbour), userID) for neighbour in neighbours]
     predict_rating_item = sum([Item_similarity_with_neighbour[i] * userRatingForNeighbour[i] for i in range(len(neighbours))]) / sum(Item_similarity_with_neighbour)
-    print(predict_rating_item)
+    return predict_rating_item
 
-predict_rating(1, 439)
+
+# calculate the MAE of the predictions
+def basic_mae():
+    c = conn.cursor()
+    c.execute( 'SELECT UserID, ItemID FROM example_table WHERE UserID = ?', (1, ))
+    user_items = c.fetchall()
+    predictedRatings = []
+
+    c.execute( 'SELECT Rating FROM example_table WHERE UserID = ?', (1, ))
+    ratings = c.fetchall()
+
+    actualRating = [rating[0] for rating in ratings]
+    print(actualRating)
+
+    for user_item in user_items:
+        predictedRatings.append(predict_rating(user_item[0], user_item[1]))
+
+    mae = sum([abs(predictedRatings[i] - actualRating[i]) for i in range(len(predictedRatings))]) / len(actualRating)
+
+    print("MAE for userID 1 = ", mae)
+
+# calculating similarity matrix for items
+def similarity_matrix_items():
+    c = conn.cursor()
+    c.execute( 'SELECT ItemID FROM example_table' )
+    duplicate_items = c.fetchall()
+    items = list(set(duplicate_items))
+    items = [item[0] for item in items]
+    similarity_matrix = np.zeros((len(items), len(items)))
+    for i in range(len(items)):
+        for j in range(len(items)):
+
+            similarity_matrix[i][j] = cosine_similarity_items(items[i], items[j], 1)
+    return similarity_matrix
